3ae24a81af9f8b6b8f46b9051253cefd93a8dd94
radicalrad1ance <jctipton123@gmail.com>
2020-05-25 22:55:27 -0400

    Add initial skeleton framework with functioning tests for Google Search

---
+++ features/step_definitions/google_search_steps.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+Given(/^I am on the Google Search page$/) do
+  @browser.goto 'https://www.google.com/'
+end
+
+When(/^I search for "([^\"]*)" (without Feeling Lucky|with Feeling Lucky)$/) do
+  |search_text, luck|
+  @browser.text_field(name: 'q').set(search_text)
+  if luck == 'with Feeling Lucky'
+    @browser.button(name: 'btnI').click
+  else
+    @browser.button(name: 'btnK').click
+  end
+end
+
+Then(/^I see results for "([^\"]*)" on the Google Results page| $/) do
+   |search_text|
+  expect(@browser.text).to include(search_text)
+end
+
+Then(/^I am taken to a "([^\"]*)" related website$/) do |search_text|
+  expect(@browser.text).to include(search_text)
+end
